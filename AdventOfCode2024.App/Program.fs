cb5682ffe227fdf1c0a78d9d4091f51c1d8d8ea5
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-12-04 22:24:43 +0000

    Fix

--- AdventOfCode2024.App/Program.fs
+++ AdventOfCode2024.App/Program.fs
@@ -1,6 +1,17 @@
 ﻿namespace AdventOfCode2024
 
+open System.IO
+
 module Program =
+    let inline input (folder : DirectoryInfo) (day : int) : string =
+        Path.Combine (folder.FullName, $"day%i{day}.txt") |> File.ReadAllText
 
     [<EntryPoint>]
-    let main argv = 0
+    let main argv =
+        let inputFolder = argv.[0] |> DirectoryInfo
+
+        do
+            let input = input inputFolder 1
+            Run.day1 true input
+
+        0
