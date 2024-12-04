c87232b99734c10b48a33490b9bbc17e0a8ed293
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2024-12-04 22:50:23 +0000

    Day 2 (#3)

---
+++ AdventOfCode2024.Test/TestDay2.fs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2024.Test
+
+open FsUnitTyped
+open AdventOfCode2024
+open NUnit.Framework
+open System.Reflection
+
+[<TestFixture>]
+module TestDay2 =
+    [<Literal>]
+    let private filename = "day2.txt"
+
+    [<Test>]
+    let ``Part 1, given example`` () =
+        let input = Assembly.readResource (Assembly.GetExecutingAssembly ()) filename
+        Day2.part1 input |> shouldEqual 2
+
+    [<Test ; Explicit>]
+    let ``Part 1`` () =
+        let input = Assembly.readResource (Assembly.GetExecutingAssembly ()) filename
+        Day2.part1 input |> shouldEqual 442
+
+    [<Test>]
+    let ``Part 2, given example`` () =
+        let input = Assembly.readResource (Assembly.GetExecutingAssembly ()) filename
+        Day2.part2 input |> shouldEqual 4
+
+    [<Test ; Explicit>]
+    let ``Part 2`` () =
+        let input = Assembly.readResource (Assembly.GetExecutingAssembly ()) filename
+        Day2.part1 input |> shouldEqual 493
---
+++ AdventOfCode2024/Day2.fs
@@ -0,0 +1,100 @@
+﻿namespace AdventOfCode2024
+
+open System
+open System.Collections.Generic
+open System.Globalization
+
+[<RequireQualifiedAccess>]
+module Day2 =
+    let getRows (input : string) =
+        let lines = input.AsSpan().Split '\n'
+        let rows = ResizeArray ()
+
+        for line in lines do
+            let line =
+                input.AsSpan().Slice (line.Start.Value, line.End.Value - line.Start.Value)
+
+            if not line.IsEmpty then
+                let row = ResizeArray ()
+
+                for i in line.Split ' ' do
+                    let i = line.Slice (i.Start.Value, i.End.Value - i.Start.Value)
+
+                    row.Add (Int32.Parse (i, CultureInfo.InvariantCulture))
+
+                rows.Add row
+
+        rows
+
+    let isSafe (indexToIgnore : int voption) (row : int ResizeArray) : bool =
+        if row.Count <= 1 + (if indexToIgnore.IsSome then 1 else 0) then
+            true
+        else
+
+        let mutable prev = row.[0 + if indexToIgnore = ValueSome 0 then 1 else 0]
+        let mutable isOk = true
+
+        let mutable i =
+            match indexToIgnore with
+            | ValueNone -> 1
+            | ValueSome j -> 1 + if j < 2 then 1 else 0
+
+        let mutable decreasing = prev >= row.[i]
+
+        while isOk && i < row.Count do
+            if indexToIgnore <> ValueSome i then
+                let curr = row.[i]
+
+                if decreasing then
+                    match curr - prev with
+                    | -1
+                    | -2
+                    | -3 -> ()
+                    | _ -> isOk <- false
+                else
+                    match curr - prev with
+                    | 1
+                    | 2
+                    | 3 -> ()
+                    | _ -> isOk <- false
+
+                prev <- curr
+
+            i <- i + 1
+
+        isOk
+
+    let part1 (input : string) =
+        let rows = getRows input
+
+        let mutable result = 0
+
+        for row in rows do
+            if isSafe ValueNone row then
+                result <- result + 1
+
+        result
+
+    let part2 (input : string) =
+        let rows = getRows input
+
+        let mutable result = 0
+
+        for row in rows do
+            if isSafe ValueNone row then
+                result <- result + 1
+            else
+
+            let mutable toIgnore = 0
+            let mutable isRowSafe = false
+
+            while not isRowSafe && toIgnore <= row.Count do
+                if isSafe (ValueSome toIgnore) row then
+                    isRowSafe <- true
+
+                toIgnore <- toIgnore + 1
+
+            if isRowSafe then
+                result <- result + 1
+
+        result
